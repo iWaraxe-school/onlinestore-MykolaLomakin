931f99decca6dd141f15ed703a15d6f6c9213570
MykolaLomakin <mykolalomakin@coherentsolutions.com>
2023-04-18 11:55:51 +0300

    builder

--- store1/src/main/java/store/populator/RandomStorePopulator.java
+++ store1/src/main/java/store/populator/RandomStorePopulator.java
@@ -6,15 +6,6 @@ import products.Product;
 public class RandomStorePopulator implements Populator {
     private final Faker faker = new Faker();
 
-    public Product generateProduct(String categoryName) {
-        Product product = Product.newProductBuilder()
-                .setName(getProductName(categoryName))
-                .setPrice(getProductPrice())
-                .setRate(getProductRate())
-                .build();
-        return product;
-    }
-
     @Override
     public String getProductName(String categoryName) {
         switch (categoryName) {
